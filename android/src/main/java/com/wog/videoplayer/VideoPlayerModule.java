4340d10987544dbecc1a57d92c9f580c76d8d98b
DC5 Admin (MACMINI032) <bondsupergoat@gmail.com>
2016-10-26 10:06:42 +0700

    - Fix react 0.35

--- android/src/main/java/com/wog/videoplayer/VideoPlayerModule.java
+++ android/src/main/java/com/wog/videoplayer/VideoPlayerModule.java
@@ -37,7 +37,7 @@ public class VideoPlayerModule extends ReactContextBaseJavaModule implements Act
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
         if (requestCode == VIDEO_CODE) {
             getCurrentActivity().finish();
         }
