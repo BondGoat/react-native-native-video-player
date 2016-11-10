02e5f332fc1baa043451c5f8d61d19eaf4f3df0e
DC5 Admin (MACMINI032) <bondsupergoat@gmail.com>
2016-11-10 15:54:44 +0700

    - Update to show control player

--- ios/RNVideoPlayer/RNVideoPlayer.m
+++ ios/RNVideoPlayer/RNVideoPlayer.m
@@ -18,19 +18,19 @@ RCT_EXPORT_METHOD(showVideoPlayer: (NSString*) url)
 {
     self.videoURL = [NSURL URLWithString:url];
     AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
-    
+
     AVPlayer *player = [AVPlayer playerWithURL:self.videoURL];
     self.playerViewController = [AVPlayerViewController new];
     _playerViewController.player = player;
-    _playerViewController.showsPlaybackControls = NO;
-    
-    
+    _playerViewController.showsPlaybackControls = YES;
+
+
     dispatch_async(dispatch_get_main_queue(), ^{
-        
+
         [delegate.window.rootViewController.view addSubview:self.playerViewController.view];
         [delegate.window.rootViewController presentViewController:self.playerViewController animated:YES completion:nil];
-        
-        _playerViewController.player.play;    
+
+        _playerViewController.player.play;
     });
 }
 
