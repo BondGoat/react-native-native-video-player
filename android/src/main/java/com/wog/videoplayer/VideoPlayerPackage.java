d1154a9787276df1211eda3e85617168c652a40e
Matías Hernández Arellano <282006+matiasfh@users.noreply.github.com>
2017-10-11 17:13:39 -0300

    Remove Override

--- android/src/main/java/com/wog/videoplayer/VideoPlayerPackage.java
+++ android/src/main/java/com/wog/videoplayer/VideoPlayerPackage.java
@@ -20,7 +20,7 @@ public class VideoPlayerPackage implements ReactPackage {
         return Arrays.<NativeModule>asList(new VideoPlayerModule(reactContext));
     }
 
-    @Override
+    
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
